1e3be5631efb82a6aeeb7979baa2dbc7770de0ff
Lucien <osbick.b@gmail.com>
2022-03-21 10:18:08 +0100

    safety commit b4 setting up redux

---
+++ client/src/stuff/1120_redux-dev-tools.js
@@ -0,0 +1,23 @@
+
+// we're in component.
+
+/**
+ * * Adding/Updating data in Redux
+ * 
+ * 1. call dispatch ----> pass an arg to it: an ActionCreator
+ * 
+ * 2. the action 
+ * 
+ * 3. the reducer (both reducer and action are in slice.js)
+ * 
+ */
+
+
+
+/**
+ * *Getting data out of Redux
+ * 
+ * useSelector ----> a hook
+ * 
+ * 
+ */
